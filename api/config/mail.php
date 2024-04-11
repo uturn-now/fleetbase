479ae936faccabe74de890785d39472a619ef01a
MaxvandeLaar <5842932+MaxvandeLaar@users.noreply.github.com>
2024-04-11 17:05:10 +0200

    feat: added default ses region

--- api/config/mail.php
+++ api/config/mail.php
@@ -49,7 +49,7 @@ return [
             'transport' => 'ses',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
         ],
 
         'mailgun' => [
